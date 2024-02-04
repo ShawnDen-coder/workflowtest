6c5f405543b64c5c691866d9e482e9cfa0b74de0
ShawnDeng-code <88145482@qq.com>
2024-02-04 21:09:31 +0800

    fix: fix deploy docs

---
+++ src/workflowtest/cfg/config.py
@@ -0,0 +1,16 @@
+import os
+
+from dynaconf import Dynaconf
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+settings = Dynaconf(
+    root_path=current_directory,
+    environments=True,
+    envvar_prefix="DYNACONF",
+    settings_files=['settings.yaml', '.secrets.yaml'],
+    load_dotenv=True
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
